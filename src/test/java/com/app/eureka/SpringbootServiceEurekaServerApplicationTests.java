7fda6d09ac1f3f25a5c5f412c44389c58b0eb3fc
Jocs <jhassan8@outlook.com>
2019-09-21 14:36:56 -0300

    Scale microservices with dynamic port

---
+++ src/test/java/com/app/eureka/SpringbootServiceEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.app.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootServiceEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
